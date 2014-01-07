5aaae83cbd3f48ea8bf82fb643b8d4b81e9e5645
park sung chan <dnrmrgks@naver.com>
2014-01-07 21:47:52 +0900

    3.4.11
    테이블 데이터 삭제 추

--- Ex.3.4.11/ViewController.m
+++ Ex.3.4.11/ViewController.m
@@ -8,16 +8,95 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>{
+    NSMutableArray *data;
+}
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
 
+
+-(BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *) alertView {
+    NSString *inputStr = [alertView textFieldAtIndex:0].text;
+    return [inputStr length] > 2;
+}
+
+-(void)alertView : (UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    if(buttonIndex == alertView.firstOtherButtonIndex) {
+        UITextField *newStr = [alertView textFieldAtIndex:0];
+        if(newStr.text.length > 0) {
+            //                        [data insertObject:newStr.text atIndex:(numOfAdd)];
+            [data addObject:newStr.text];
+            NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([data count]-1) inSection:0];
+            NSArray *row = [NSArray arrayWithObject:indexPath];
+            [self.table insertRowsAtIndexPaths:row withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+        
+    }
+    
+}
+
+
+
+- (IBAction)toggleEdit:(id)sender {
+    
+    _table.editing = !_table .editing;
+}
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if(indexPath.row %3 == 0 ){
+        return UITableViewCellEditingStyleNone;
+    }
+    else if(indexPath.row %3 == 1){
+        return UITableViewCellEditingStyleDelete;
+    }
+    else{
+        return UITableViewCellEditingStyleInsert;
+    }
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(UITableViewCellEditingStyleDelete == editingStyle){
+        [data removeObjectAtIndex:indexPath.row];
+        
+        NSArray *rowList =  [NSArray arrayWithObject:indexPath];
+        [tableView deleteRowsAtIndexPaths:rowList withRowAnimation:UITableViewRowAnimationAutomatic];
+        
+    }
+    else{
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"추가" message:Nil delegate:self cancelButtonTitle:@"취소" otherButtonTitles:@"확인", nil];
+        alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+        [alert show];
+        
+    }
+    
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"CELL_ID"];
+    cell.textLabel.text = data [indexPath.row];
+    return cell;
+}
+
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    data =[[NSMutableArray alloc]initWithObjects:@"Item0",@"Item1",@"Item2",@"Item3",@"Item4",@"Item5",@"Item6",@"Item7" ,nil];
 }
 
 - (void)didReceiveMemoryWarning
